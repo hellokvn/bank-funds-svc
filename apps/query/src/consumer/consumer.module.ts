1be3e8911c0d9e6ad5dedfd0ecefd930e880bd94
Kevin <hellokevinvogel@gmail.com>
2022-04-23 13:14:04 +0200

    chore(query): add get balance lookup module

---
+++ apps/query/src/consumer/consumer.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { FundsDepositedModule } from './funds-deposited/funds-deposited.module';
+import { FundsTransferedModule } from './funds-transfered/funds-transfered.module';
+import { FundsWithdrawnModule } from './funds-withdrawn/funds-withdrawn.module';
+
+@Module({
+  imports: [FundsDepositedModule, FundsTransferedModule, FundsWithdrawnModule]
+})
+export class ConsumerModule {}
---
+++ apps/query/src/lookup/lookup.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { GetBalanceModule } from './get-balance/get-balance.module';
+
+@Module({
+  imports: [GetBalanceModule]
+})
+export class LookupModule {}
